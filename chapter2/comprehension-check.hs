13e3a7eefca041412cd4a85d31227fa2e9739547
Abhigya Maskay <ave70011@gmail.com>
2024-11-17 16:58:15 -0500

    Added the comprehension check exercise

---
+++ chapter2/comprehension-check.hs
@@ -0,0 +1,7 @@
+-- Writing a function from the give expressions
+timesPi :: Double -> Double
+timesPi x = 3.14 * x
+
+-- Rewritten using the prelude version
+timesPi2 :: Double -> Double
+timesPi2 x = pi * 3.14
