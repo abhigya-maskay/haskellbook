a709d0c29d9d17494bdb43c532638b052a32df97
Abhigya Maskay <ave70011@gmail.com>
2024-11-20 22:37:45 -0500

    Added the first two exercises from chapter 4

---
+++ chapter4/find-the-mistakes.hs
@@ -0,0 +1,14 @@
+-- 1.
+x = not (True && True)
+
+-- 2.
+y = not (x == 6)
+
+-- 3. No mistakes in this one
+z = (1 * 2) > 5
+
+-- 4.
+a = ["Merry"] > ["Happy"]
+
+-- 5.
+b = ['a', 'b', 'c'] ++ "look at me!"
---
+++ chapter4/mood-swing.hs
@@ -0,0 +1,9 @@
+data Mood = Blah | Woot deriving Show
+
+-- 1.The type constructor here is Mood
+-- 2.The Mood values that you could possibly use are Blah and Woot
+-- 3. changeMood should be a function from Mood -> Mood
+
+changeMood :: Mood -> Mood
+changeMood Blah = Woot
+changeMood Woot = Blah
