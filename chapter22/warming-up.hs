c8379e16c95783ad8a682debd08c938b4574e7d8
Abhigya Maskay <ave70011@gmail.com>
2025-07-27 18:37:59 -0400

    Finished chapter 22 exercises

---
+++ chapter22/warming-up.hs
@@ -0,0 +1,21 @@
+module WarmingUp where
+
+import Data.Char
+
+cap :: [Char] -> [Char]
+cap xs = map toUpper xs
+
+rev :: [Char] -> [Char]
+rev xs = reverse xs
+
+composed :: [Char] -> [Char]
+composed = cap . rev
+
+fmapped :: [Char] -> [Char]
+fmapped = fmap cap rev
+
+tupled :: [Char] -> ([Char], [Char])
+tupled = (,) <$> cap <*> rev
+
+tupled' :: [Char] -> ([Char], [Char])
+tupled' = cap >>= \ c -> rev >>= \ r -> return (c, r)
